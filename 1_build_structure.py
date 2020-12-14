610b6c1b158f07a1d6a171bc230304e559e8e00e
rodivoo <ronald.malis@gmail.com>
2020-12-14 22:18:00 +0100

    homework09

---
+++ 1_build_structure.py
@@ -0,0 +1,24 @@
+import functions
+
+###########################################################
+# VARIABLES ###############################################
+###########################################################
+
+settingsFile = "./settings.yml"
+settings = functions.loadYmlSettings(settingsFile)
+
+bibAll = settings["bib_all"]
+memexPath = settings["path_to_memex"]
+
+###########################################################
+# PROCESS ALL RECORDS #####################################
+###########################################################
+
+def processAllRecords(bibData):
+    for k,v in bibData.items():
+        functions.processBibRecord(memexPath, v)
+
+bibData = functions.loadBib(bibAll)
+processAllRecords(bibData)
+
+print("Done!") 
